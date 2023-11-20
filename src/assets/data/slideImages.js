9b36e268bad0e07e228547fd990df29504c108f6
AZthChowk <sambudhapp@gmail.com>
2023-11-20 09:19:09 +0545

    change in image slider image index

--- src/assets/data/slideImages.js
+++ src/assets/data/slideImages.js
@@ -1,11 +1,11 @@
 export const slides = [
   {
-    url: "public/bg.png",
-    title: "boat",
-  },
-  {
     url: "https://images.unsplash.com/photo-1541462608143-67571c6738dd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "beach",
+  },
+  {
+    url: "public/bg.png",
+    title: "boat",
   },
   {
     url: "https://images.pexels.com/photos/1269808/pexels-photo-1269808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
