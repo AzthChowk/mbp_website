28226f36a5ac870b58c51e987d3ff20e275ddcd8
AZthChowk <sambudhapp@gmail.com>
2023-12-07 12:30:37 +0545

    fix download source

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
   const onButtonClick = () => {
-    const pdfUrl = "/public/ManakamanaBindingPress Profile.pdf";
+    const pdfUrl = "/ManakamanaBindingPress Profile.pdf";
     const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = "/public/ManakamanaBindingPress Profile.pdf"; // specify the filename
+    link.download = "/ManakamanaBindingPress Profile.pdf"; // specify the filename
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
